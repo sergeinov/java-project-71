78eac15a323a0936e9db1a04669850303bdec14f
Sergei <test@gmail.com>
2023-08-20 18:16:32 +0200

    add help output

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,9 +1,26 @@
 package hexlet.code;
 
-public class App {
-    public static void main(String[] args) {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
 
-        System.out.printf("Hello project %s", "App");
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "1.0", description = "Compares two configuration files and shows a difference.")
+public class App implements Runnable {
+    @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this @|fg(cyan) help|@ message and exit.")
+    private boolean helpRequested;
+
+    @Option(names = {"-V", "--version"}, versionHelp = true, description = "Print version information and exit.")
+    private boolean versionRequested;
+
+    public static void main(String[] args) {
+        // Создаем объект командной строки и выполняем execute - парсим аргументы
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+
+    }
+
+    @Override
+    public void run() {
 
     }
 }
