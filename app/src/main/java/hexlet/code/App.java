2d6ba69787e7c2cfb68376c31c083fd557c31604
Sergei <test@gmail.com>
2023-08-25 01:14:09 +0200

    add picocli help

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,13 +3,48 @@ package hexlet.code;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
-@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "1.0", description = "Compares two configuration files and shows a difference.")
+@Command(
+        name = "gendiff",
+        mixinStandardHelpOptions = true,
+        version = "1.0",
+        description = "Compares two configuration files and shows a difference."
+)
 public class App implements Runnable {
-    @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this @|fg(cyan) help|@ message and exit.")
+    @Parameters(
+            index = "0",
+            paramLabel = "filepath1",
+            description = "path to first file"
+    )
+    String filepath1;
+
+    @Parameters(
+            index = "1",
+            paramLabel = "filepath2",
+            description = "path to second file"
+    )
+    String filepath2;
+
+    @Option(
+            names = {"-f", "--format"},
+            defaultValue = "stylish",
+            description = "output format [default: ${DEFAULT-VALUE}]"
+    )
+    private String format;
+
+    @Option(
+            names = {"-h", "--help"},
+            usageHelp = true,
+            description = "Show this @|fg(cyan) help|@ message and exit."
+    )
     private boolean helpRequested;
 
-    @Option(names = {"-V", "--version"}, versionHelp = true, description = "Print version information and exit.")
+    @Option(
+            names = {"-V", "--version"},
+            versionHelp = true,
+            description = "Print version information and exit."
+    )
     private boolean versionRequested;
 
     public static void main(String[] args) {
